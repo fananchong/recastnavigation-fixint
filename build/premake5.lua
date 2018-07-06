8051b20ab7cfdd049b467fb0458c2e3508e5336e
fananchong <fananchong@126.com>
2018-07-06 16:35:29 +0800

    recastnavigation代码

---
+++ build/premake5.lua
@@ -0,0 +1,69 @@
+workspace "recastnavigation-fixint"
+    configurations { "Debug", "Release" }
+    platforms { "x32", "x64" }
+    targetdir "../bin/"
+    language "C++"
+    includedirs {
+        "..",
+        "../Contrib",
+        "../Contrib/fastlz",
+        "../Contrib/SDL/include",
+        "../DebugUtils/Include",
+        "../Detour/Include",
+        "../DetourCrowd/Include",
+        "../DetourTileCache/Include",
+        "../Recast/Include/",
+        "../RecastDemo/Include/",
+    }
+    flags {
+        "C++11",
+        "StaticRuntime",
+    }
+
+    filter "configurations:Debug"
+    defines { "_DEBUG" }
+    flags { "Symbols" }
+    libdirs { }
+    filter "configurations:Release"
+    defines { "NDEBUG" }
+    libdirs { }
+    optimize "On"
+    filter { }
+    
+
+if os.is("windows") then
+project "recast"
+    kind "ConsoleApp"
+    targetname "recast"
+    libdirs { "../bin" }
+    files {
+        --"../Contrib/**",
+        "../DebugUtils/**",
+        "../Detour/**",
+        "../DetourCrowd/**",
+        "../DetourTileCache/**",
+        "../Recast/**",
+        "../RecastDemo/**",
+    }
+    defines { "WIN32", "_WINDOWS", "_CRT_SECURE_NO_WARNINGS", "_HAS_EXCEPTIONS=0" }
+    
+    configuration { "windows" }
+	links { 
+		"glu32",
+		"opengl32",
+		"SDL2",
+		"SDL2main",
+	}
+    filter { "platforms:x32" }
+        libdirs { "../Contrib/SDL/lib/x86" }
+        postbuildcommands {
+            -- Copy the SDL2 dll to the Bin folder.
+            '{COPY} "%{wks.location}../Contrib/SDL/lib/x86/SDL2.dll" "%{cfg.targetdir}"'
+        }
+    filter { "platforms:x64" }
+        libdirs { "../Contrib/SDL/lib/x64" }
+        postbuildcommands {
+            -- Copy the SDL2 dll to the Bin folder.
+            '{COPY} "%{wks.location}../Contrib/SDL/lib/x64/SDL2.dll" "%{cfg.targetdir}"'
+        }
+end
